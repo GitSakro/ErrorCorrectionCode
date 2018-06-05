4b41d4cc451c97cc913eebf45fd852ef41241bd1
Arkadiusz Jastrzebski <arkadiusz.jastrzebski@ericsson.com>
2018-06-05 21:42:01 +0200

    recatngles

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -3,29 +3,41 @@ package app;
 import CommunicationFlow.Channel;
 import CommunicationFlow.Receiver;
 import CommunicationFlow.Transmitter;
+import JavaFxHelpers.Shapes;
 import algorithm.TripleRepetitionCode;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(e -> {
-            System.out.println("Hello World!");
-        });
-        transmision();
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Rectangle rectangle = Shapes.createRectangle(50.0f,75.0f,300.0f,150.0f);
+        Rectangle rectangle2 = Shapes.createRectangle(650.0f,75.0f,300.0f,150.0f);
 
-        Scene scene = new Scene(root, 300, 250);
+        rectangle.setFill(Paint.valueOf("yellow"));
+        StackPane rectanglePane = new StackPane();
+        rectanglePane.getChildren().add(rectangle);
+        rectanglePane.getChildren().add(new Label("Kotek"));
+        //Creating a Group object
+        Group root = new Group(rectanglePane);
+        root.getChildren().add(rectangle2);
+        //Creating a scene object
+        Scene scene = new Scene(root, 1000, 300);
 
-        primaryStage.setTitle("Hello World!");
+        //Setting title to the Stage
+        primaryStage.setTitle("Drawing a Rectangle");
+
+        //Adding scene to the stage
         primaryStage.setScene(scene);
+
+        //Displaying the contents of the stage
         primaryStage.show();
     }
 
